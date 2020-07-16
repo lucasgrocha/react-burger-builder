3191575e8da42d8391a12c48784815923aa6bdbb
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-07-16 15:58:32 -0300

    181. Sending a POST Request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burguer from '../../components/Burger/Burguer'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import axios from '../../axios-order'
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -84,7 +85,25 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('You continue!')
+    // alert('You continue!')
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Lucas R',
+        address: {
+          street: 'TestStreet',
+          zipCode: '12312312',
+          country: 'Brazil'
+        },
+        email: 'test@email.com'
+      },
+      deliveryMethod: 'fatest'
+    }
+
+    axios.post('/orders.json', order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error))
   }
 
   render() {
